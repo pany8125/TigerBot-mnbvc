f1238a3386f6210a0275d0941dd43ad4fe7715b4
Scott <pany-8125@163.com>
2023-11-24 22:46:22 +0800

    tigerbot 微调数据标准化完成

--- schema.py
+++ schema.py
@@ -1,13 +1,13 @@
 import json
 from datetime import datetime
 
-class ShareGPTQASchema:
+class TigerBotQASchema:
 
     def __init__(self, id, question, answer, question_detail, answer_detail, session, round_number, model, other_field):
         self.id = id
         self.question = question
         self.answer = answer
-        self.source = "ShareGPT"
+        self.source = "TigerBot"
         self.create_time = datetime.now().strftime("%Y%m%d %H:%M:%S")
         self.question_detail = question_detail 
         self.answer_detail = answer_detail 
@@ -31,20 +31,50 @@ class ShareGPTQASchema:
         # 扩展字段直接json dump
         return json.dumps(data, separators=(",", ":"), ensure_ascii=False)
 
+class TigerBotTextSchema:
 
-# json_str = ShareGPTQASchema(0, "Can you make me a Shakespearean script about a girl who has tummy troubles and can't fart not matter how hard she tries- so they think she is a witch",
-#                 "Sure, here's a Shakespearean script about a girl who c...", "ShareGPT", 1).to_json()
-# print(json_str)
-unique_id = 0
-question = 'aaa'
-i = 5
-question_detail = "\"from\": \"human\""
-answer_detail = "\"from\": \"gpt\""
-id = 'aaa'
-model = 'gpt4'
-gpt4_json = ShareGPTQASchema(unique_id, question, '', question_detail, answer_detail, id, i, model, '')
-gpt4_json.source = 'xxx'
-# print(gpt4_json.to_json()) 
+    def __init__(self, file_name, file_size, extended_field, paragraps):
+        self.file_name = file_name
+        self.file_size = file_size
+        self.extended_field = extended_field
+        self.paragrap = paragraps
 
-import datasets
-ds_sft = datasets.load_dataset('TigerResearch/tigerbot-research-plugin')
+    def to_json(self):
+        data = {
+                "文件名": self.file_name,
+                "是否待查文件": False,
+                "是否重复文件": False,
+                "文件大小": self.file_size,
+                "simhash": 0,
+                "最长段落长度": 0,
+                "段落数": 0,
+                "去重段落数": 0,
+                "低质量段落数": 0,
+                '扩展字段': self.extended_field,
+                "段落": self.paragrap
+            }
+        return json.dumps(data, separators=(",", ":"), ensure_ascii=False)
+
+# TigerBotText段落Schema
+class TigerBotTextParagraphSchema:
+
+    def __init__(self, line_number, md5, title, author, content, extended_field):
+        self.line_number = line_number
+        self.md5 = md5
+        self.title = title
+        self.author = author
+        self.content = content
+        self.extended_field = extended_field
+
+    def to_json(self):
+        data = {
+                    '行号': self.line_number,
+                    '是否重复': False,
+                    '是否跨文件重复': False,
+                    'md5': self.md5, #整行json的md5
+                    '标题': self.title, # 对应title
+                    '作者': self.author, # 对应author
+                    '内容': self.content, # 对应content
+                    '扩展字段': self.extended_field # 对应content
+                }
+        return json.dumps(data, separators=(",", ":"), ensure_ascii=False)
--- tiger_bot_extract.py
+++ tiger_bot_extract.py
@@ -2,16 +2,22 @@
 # 脚本设置使用utf-8编码
 
 from datetime import datetime
+from glob import glob
 import json
 from enum import Enum
+import os
 import schema
 import hashlib
 import logging
 import re
+import chardet
+
+SOURCE = 'TigerBot'
+OUTPUT_DIR = ''
 
 # 配置日志记录
 logging.basicConfig(
-    filename='shareGTP_log_file.log',  # 指定日志文件的名称
+    filename='TigerBot_log_file.log',  # 指定日志文件的名称
     level=logging.INFO,  # 指定日志级别（INFO、WARNING、ERROR、CRITICAL等）
     format='%(asctime)s [%(levelname)s]: %(message)s',  # 日志格式
     datefmt='%Y-%m-%d %H:%M:%S'  # 日期和时间格式
@@ -478,39 +484,127 @@ def process_json_baiduzhidao(json_str, write_file=None, model='common_en'):
         logging.error("KeyError")
         return False
 
+# 处理 TigerBot 的数据
+
+def tiger_bot_extract(f_path, output_file, type_str, max_size):
+    if type_str == 'fine-tuning':
+        # 调用函数来处理 JSON 文件，默认从第1行开始读取
+        process_qa(f_path, output_file, max_size)
+    elif type_str == 'domain':
+        # 调用函数来处理 JSON 文件，默认从第1行开始读取
+        process_domain_text(f_path, output_file, max_size)
+    elif type_str == 'pretraining':
+        # 调用函数来处理 JSON 文件，默认从第1行开始读取
+        process_pretraining_text(f_path, output_file, max_size)
+    else:
+        logging.error(f"type: {type_str} not support!")
+
+def process_qa(f_path, output_file, max_size):
+    with open(f_path, 'r', encoding='utf-8') as f:
+        # 读取第一行
+        line = f.readline().strip('\n').replace('\ufeff', '')
+        # 判断第一行是否为一个有效的json
+        try:
+            json_data = json.loads(line)
+            # 按jsonl格式处理文件
+            process_qa_file_common(f_path, output_file, max_size)
+        except json.JSONDecodeError:
+            line = f.readline()
+            logging.warning("JSONDecodeError")
+            logging.warning(line)
+            # 按手动解析的方式处理文件
+            process_qa_manual(f_path, output_file, max_size)
+
+def process_qa_file_common(f_path, output_file, max_size):
+    # 文件序号
+    file_number = 1
+    write_file = open(OUTPUT_DIR + f'{output_file}_{file_number:02}.jsonl', 'w', encoding='utf-8')
+    with open(f_path, 'r', encoding='utf-8') as f:
+        for line_number, line in enumerate(f, start=1):
+            # 打印迭代信息
+            logging.debug(f"Line {line_number}: {line}")
+            this_line = line.strip('\n').replace('\ufeff', '')
+            if process_qa_json_common(this_line, write_file):
+                this_line = ''
+            else:
+                logging.error("common json parse error!")
+                logging.error(f"Line {line_number}, error!")  # json检测失败
+                logging.error(f"json str: {this_line}")
+                break
+            # 如果文件超过500M，就关闭文件，新建文件
+            if write_file.tell() > max_size:
+                write_file.close()
+                file_number += 1
+                write_file = open(OUTPUT_DIR + f'{output_file}_{file_number:02}.jsonl', 'w', encoding='utf-8')
+
+def process_qa_manual(f_path, output_file, max_size):
+    logging.warning("process_qa_manual")
+    logging.warning(f"file: {f_path}")
+    pass
+
+def process_qa_json_common(json_str, write_file=None):
+    # Check if str is a valid JSON
+    try:
+        json_data = json.loads(json_str)
+        logging.debug(json_data)
+        #用json_data的md5值作为id
+        unique_id = hashlib.md5(json_str.encode('utf-8')).hexdigest()
+        question_detail = "\"instruction\"\"input\""
+        answer_detail = "\"output\""
+        # 取出json中instruction对应的字符串，None转成空字符串
+        question = json_data['instruction'] if json_data['instruction'] != None else ''
+        question += json_data['input'] if json_data['input'] != None else ''
+        answer = json_data['output'] if json_data['output'] != None else ''
+        json_schema = schema.TigerBotQASchema(unique_id, question, answer, question_detail, answer_detail, unique_id, 1, f'\"{SOURCE}\"', '')
+        # 生成json
+        json_str = json_schema.to_json()
+        write_file.write(json_str)
+        write_file.write('\n')
+        return True
+    except json.JSONDecodeError:
+        logging.error("JSONDecodeError")
+        return False
+    except KeyError:
+        logging.error("KeyError")
+        return False
+
+def process_domain_text():
+    pass
+
+def process_pretraining_text():
+    pass
+
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="Parse shareGPT into QA data.")
-    parser.add_argument("source_files",type=str, default="final_data_sample_230706test.json", help="文件名")
-    parser.add_argument("-o","--output",type=str, default="shareGPT",help="output file name (without extension)")
-    parser.add_argument("-l","--start_line",type=int,default=1,help="read start line")
+    parser = argparse.ArgumentParser(description="Parse TigerBot data.")
+    parser.add_argument("source_files",type=str, default="final_data_sample_230706test.json", help="文件名或者目录名")
+    parser.add_argument("dest_dir",type=str, default="final_data_sample_230706test", help="文件名或者目录名")
     parser.add_argument("-s","--max_size",type=int,default=500 * 1024 * 1024,help="max chunk size")
-    # model：
-    #   gpt4：https://huggingface.co/datasets/Ejafa/GPT_4_with_ShareGPT/tree/main
-    #   common：https://huggingface.co/datasets/shareAI/ShareGPT-Chinese-English-90k/tree/main/sharegpt_jsonl
-    #   multilang：https://huggingface.co/datasets/cryscan/multilingual-share/tree/main
-    #   vicuna：https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/tree/main
-    #   baiduzhidao：baiduzhidao-train.jsonl
-    parser.add_argument("-m","--model",type=str,default='gpt4',help="multi model parse")
+    # type的类型
+    # fine-tuning: 用于fine-tuning的数据
+    # domain: 用于domain的数据
+    # pretraining: 用于pretraining的数据
+    parser.add_argument("-m","--type",type=str,default='fine-tuning',help="multi model parse")
     args = parser.parse_args()
     # 当前时间
     cur_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    of = open(f'{args.output}_{args.model}_{cur_time}.jsonl', 'w', encoding='utf-8')
-    if args.model == 'vicuna' or args.model == 'gpt4':
+    file_paths = []
+    OUTPUT_DIR = args.dest_dir
+    # 检查文件是否存在
+    if os.path.isfile(args.source_files):
+        file_paths.append(args.source_files)
+    elif os.path.isdir(args.source_files):
+        for f_path in sorted(glob(f"{args.source_files}/**/*.json*", recursive=True)):
+            if not os.path.isfile(f_path):
+                # 跳过子目录并打印日志
+                logging.warning(f"Skipping subdirectory {f_path}")
+                continue
+            file_paths.append(f_path)
+    # 打印处理清单
+    logging.info(f"Processing {len(file_paths)} files")
+    for f_path in file_paths:
+        logging.info(f"Processing {f_path}")
+        f_prefix_name = os.path.basename(f_path).split('.')[0]
+        output_file_prefix = f'{SOURCE}_{args.type}_{f_prefix_name}_{cur_time}'
         # 调用函数来处理 JSON 文件，默认从第1行开始读取
-        process_json_file_gpt4(args.source_files, of, args.start_line, args.model)
-    elif args.model == 'multilang':
-        logging.info(f"model: {args.model}")
-        # 调用函数来处理 JSON 文件，默认从第1行开始读取
-        process_json_file_multilang(args.source_files, of, args.start_line)
-    elif args.model == 'common_en' or args.model == 'common_zh':
-        logging.info(f"model: {args.model}")
-        # 调用函数来处理 JSON 文件，默认从第1行开始读取
-        process_json_file_common(args.source_files, of, args.start_line, args.model)
-    elif args.model == 'baiduzhidao':
-        logging.info(f"model: {args.model}")
-        # 调用函数来处理 JSON 文件，默认从第1行开始读取
-        process_json_file_baiduzhidao(args.source_files, of, args.start_line, args.model, cur_time, args.max_size, args.output)
-    else:
-        logging.error(f"model: {args.model} not support!")
-    of.close()
+        tiger_bot_extract(f_path, output_file_prefix, args.type, args.max_size)
